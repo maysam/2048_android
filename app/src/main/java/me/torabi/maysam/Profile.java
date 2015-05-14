7c0fa56f936f0c94e5bffa80fc4582339ae5ace6
maysam <kherad@gmail.com>
2015-05-15 01:14:52 +0430

    implementing swipes

--- app/src/main/java/me/torabi/maysam/Profile.java
+++ app/src/main/java/me/torabi/maysam/Profile.java
@@ -15,20 +15,42 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class Profile extends Activity {
 
-    CanvasView canvasView  = null;
+    private CanvasView canvasView  = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (canvasView == null)
+        if (canvasView == null) {
             canvasView = new CanvasView(this);
+            canvasView.setOnTouchListener(new OnSwipeTouchListener(this) {
+                public void onSwipeTop() {
+                    Toast.makeText(Profile.this, "top", Toast.LENGTH_SHORT).show();
+                }
+                public void onSwipeRight() {
+                    Toast.makeText(Profile.this, "right", Toast.LENGTH_SHORT).show();
+                }
+                public void onSwipeLeft() {
+                    Toast.makeText(Profile.this, "left", Toast.LENGTH_SHORT).show();
+                }
+                public void onSwipeBottom() {
+                    Toast.makeText(Profile.this, "bottom", Toast.LENGTH_SHORT).show();
+                }
+
+                public boolean onTouch(View v, MotionEvent event) {
+                    return gestureDetector.onTouchEvent(event);
+                }
+            });
+
+        }
         setContentView(canvasView);
         Log.e("Prof", "onCreate");
     }
